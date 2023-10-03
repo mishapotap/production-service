a6ce4ba53d083056918ed8819a9aaec3228fbfa4
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-10-03 12:21:55 +0300

    Added NotFoundPage and PageLoader

---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames';
+import { Loader } from 'shared/ui/Loader/Loader';
+import styles from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+    className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+    return (
+        <div className={classNames(styles.pageLoader, {}, [className])}>
+            <Loader />
+        </div>
+    );
+};
