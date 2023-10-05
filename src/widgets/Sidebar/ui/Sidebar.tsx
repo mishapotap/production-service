5fc46193d79d888419c3a85f6470ed52e9bfa3ea
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-10-05 11:26:22 +0300

    Added translation for Sidebar button

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -4,18 +4,20 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
     className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+    const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState<boolean>(false);
     const onToggle = () => setCollapsed((prev) => !prev);
     return (
         <div data-testid="sidebar" className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [className])}>
             <Button data-testid="sidebar-toggle" onClick={onToggle}>
-                TOGGLE
+                {t('Переключить')}
             </Button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
