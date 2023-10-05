9c7cd7eb9efce6ef120ca7fcbe1b358be360f02b
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-10-05 11:32:09 +0300

    Added translation for Navbar buttons

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,23 @@
 import styles from './Navbar.module.scss';
 import { classNames } from 'shared/lib/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
     return (
         <div className={classNames(styles.navbar, {}, [className])}>
             <div className={styles.links}>
                 <AppLink to="/" theme={AppLinkTheme.SECONDARY}>
-                    Главная
+                    {t('Главная')}
                 </AppLink>
                 <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                    О сайте
-                </AppLink>{' '}
+                    {t('О сайте')}
+                </AppLink>
             </div>
         </div>
     );
